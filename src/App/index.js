8f642fb0ec09a893130ae88688a60f77f4fbe097
Jesus Vargas <jesusvarguer18@gmail.com>
2023-01-12 00:26:40 -0500

    Composicion y colocacion de estado en React

--- src/App/index.js
+++ src/App/index.js
@@ -1,90 +1,54 @@
 import React from 'react';
-import { useTodos } from './useTodos';
-import { TodoHeader } from '../TodoHeader';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { TodosError } from '../TodosError';
-import { TodosLoading } from '../TodosLoading';
-import { EmptyTodos } from '../EmptyTodos';
-import { TodoForm } from '../TodoForm';
-import { CreateTodoButton } from '../CreateTodoButton';
-import { Modal } from '../Modal';
-import { ChangeAlert } from '../ChangeAlert';
+// import { TodoProvider } from '../TodoContext';
+// import { TodoCounter } from '../TodoCounter';
+// import { AppUI } from './AppUI';
 
 function App() {
-  const {
-    error,
-    loading,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-    openModal,
-    setOpenModal,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    addTodo,
-    sincronizeTodos,
-  } = useTodos();
-  
+  const [state, setState] = React.useState('estado compartido');
+
   return (
     <React.Fragment>
-      <TodoHeader loading={loading}>
-        <TodoCounter
-          totalTodos={totalTodos}
-          completedTodos={completedTodos}
-        />
-        <TodoSearch
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-        />
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
       </TodoHeader>
 
-      <TodoList
-        error={error}
-        loading={loading}
-        totalTodos={totalTodos}
-        searchedTodos={searchedTodos}
-        searchText={searchValue}
-        onError={() => <TodosError />}
-        onLoading={() => <TodosLoading />}
-        onEmptyTodos={() => <EmptyTodos />}
-        onEmptySearchResults={
-          (searchText) => <p>No hay resultados para {searchText}</p>
-        }
-      >
-        {todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        )}
+      <TodoList>
+        <TodoItem state={state} />
       </TodoList>
-
-      {!!openModal && (
-        <Modal>
-          <TodoForm
-            addTodo={addTodo}
-            setOpenModal={setOpenModal}
-          />
-        </Modal>
-      )}
-
-      <CreateTodoButton
-        setOpenModal={setOpenModal}
-      />
-
-      <ChangeAlert
-        sincronize={sincronizeTodos}
-      />
     </React.Fragment>
   );
 }
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+function TodoList({ children }) {
+  return (
+    <section className="TodoList-container">
+      {children}
+    </section>
+  );
+}
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
