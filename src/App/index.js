bd5d8bd1869d4525f694841367df4d1cc99c450b
Jesus Vargas <jesusvarguer18@gmail.com>
2023-01-18 00:25:08 -0500

    Cambiando HOCs por React Hooks

--- src/App/index.js
+++ src/App/index.js
@@ -11,7 +11,7 @@ import { EmptyTodos } from '../EmptyTodos';
 import { TodoForm } from '../TodoForm';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { Modal } from '../Modal';
-import { ChangeAlertWithStorageListener } from '../ChangeAlert';
+import { ChangeAlert } from '../ChangeAlert';
 
 
 function App() {
@@ -90,7 +90,7 @@ function App() {
         setOpenModal={setOpenModal}
       />
 
-      <ChangeAlertWithStorageListener
+      <ChangeAlert
         sincronize={sincronizeTodos}
       />
     </React.Fragment>
